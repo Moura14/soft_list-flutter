5cc4cef6c1513d2cb876e23267576feccf45e1e8
João Gabriel <jgmoura.dev@gmail.com>
2023-05-10 12:19:15 -0300

    leitura e salvamento de arquivos

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +13,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final _toDoController = TextEditingController();
   final List _toDoList = [];
+  Map<String, dynamic> _lastRemove = {};
+  int _lastRemovePos = 0;
 
   void _addToDo() {
     setState(() {
@@ -25,7 +30,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
         title: const Text('Lista de compras'),
         centerTitle: true,
       ),
@@ -49,6 +54,7 @@ class _HomePageState extends State<HomePage> {
                   },
                   icon: const Icon(
                     Icons.add_shopping_cart,
+                    color: Colors.blue,
                   ),
                 ),
               ],
@@ -87,9 +93,53 @@ class _HomePageState extends State<HomePage> {
         onChanged: (c) {
           setState(() {
             _toDoList[index]['ok'] = c;
+            saveData();
           });
         },
       ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemove = Map.from(_toDoList[index]);
+          _lastRemovePos = index;
+          _toDoList.removeAt(index);
+
+          saveData();
+
+          final snack = SnackBar(
+            content: Text('Item ${_lastRemove['title']} removida'),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovePos, _lastRemove);
+                });
+              },
+            ),
+            duration: const Duration(seconds: 2),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snack);
+        });
+      },
     );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> saveData() async {
+    String data = jsonEncode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return '';
+    }
   }
 }
